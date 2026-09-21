c4231bfb3518d434111c80bb9bf34a68b752960e
framefutures-wq <framefutures@gmail.com>
2026-09-22 02:09:22 +0900

    feat: mount region quick-switch drawer

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import RegionQuickSwitch from "./RegionQuickSwitch";
 import "./styles.css";
 import "./redesign.css";
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <RegionQuickSwitch />
     <App />
   </React.StrictMode>,
 );
