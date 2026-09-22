1e2046f2a325c8873942431ecc39185a8a055360
framefutures-wq <framefutures@gmail.com>
2026-09-22 09:03:36 +0900

    feat: mount category discovery drawer

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import RegionQuickSwitch from "./RegionQuickSwitch";
+import CategoryQuickSwitch from "./CategoryQuickSwitch";
 import "./styles.css";
 import "./redesign.css";
 import "./region-drawer.css";
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RegionQuickSwitch />
+    <CategoryQuickSwitch />
     <App />
   </React.StrictMode>,
 );
